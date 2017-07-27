56551115ead3fda82e50e705b99553476f939a1f
zcwist <zcwist@gmail.com>
2017-07-27 12:50:49 -0700

    add T9

--- Scriptor.py
+++ Scriptor.py
@@ -20,7 +20,7 @@ class Scriptor(object):
 
 if __name__ == '__main__':
 	fakedata =  {"survey_name":"Module 1",
-				"pre_questions":["What's your name?","What class are you in?"],
+				"pre_questions":["What's your name?"],
 				"methods":["AEIOU", "POEMS"],
 				"questions":[
 							{"type":"multiple_choice","question_text":"Were you familiar with this method before using theDesignExchange?","choice_items":["Yes","No"]},
@@ -28,6 +28,51 @@ if __name__ == '__main__':
 							{"type":"text","question_text":"Why did you choose or not choose this method?"}
 							]
 				}
-	print Scriptor(fakedata).scriptRender()
+	fakedata =  {"survey_name":"I3 Survey- Module 1: Research",
+				"pre_questions":["What's your name?"],
+				"methods":["AEIOU","POEMS","POSTA","Closed Card Sorting","Open Card Sorting","Design Ethnography","Focus Group","Community Appraisal","Conversation Cafe","Competitive Analysis","Conjoint Analysis","1:1 Interview","User Observation","Usability Testing"],
+				"questions":[
+							{"type":"multiple_choice","question_text":"Were you familiar with this method before using theDesignExchange?","choice_items":["Yes","No"]},
+							{"type":"multiple_choice","question_text":"Did you choose this method?","choice_items":["Yes","No"]},
+							{"type":"text","question_text":"Why did you choose or not choose this method?"}
+							]
+				}
+	survey2 = 	{"survey_name":"I5 Survey- Module 2: Analysis",
+				"pre_questions":["What's your name?"],
+				"methods":["Why-how Laddering","Empathy Map","Spectrum Mapping","2x2 Matrix","Reframing","Powers of 10","Customer Journey Mapping","How Might We","Mind Map","Contextmapping","Affinity","Diagramming","Atomize","Concept Map","Touchpoint Matrix","Task Analysis","Kano Analysis"],
+				"questions":[
+							{"type":"multiple_choice","question_text":"Were you familiar with this method before using theDesignExchange?","choice_items":["Yes","No"]},
+							{"type":"multiple_choice","question_text":"Did you choose this method?","choice_items":["Yes","No"]},
+							{"type":"text","question_text":"Why did you choose or not choose this method?"}
+							]
+				}	
+	survey3 = 	{"survey_name":"I6 Survey- Module 3: Ideate",
+				"pre_questions":["What's your name?"],
+				"methods":["6-Up Sketches","Visual Brainstorming","Brainstorming","3-12-3 Brainstorming","6-3-5 Brainwriting","Attribute Listing","Do-Redo- Undo","Biomimicry","Weighted Matrix","Forced Analogy","Design Heuristics","The Anti-Problem","Borda Count Voting","Design the Box"],
+				"questions":[
+							{"type":"multiple_choice","question_text":"Were you familiar with this method before using theDesignExchange?","choice_items":["Yes","No"]},
+							{"type":"multiple_choice","question_text":"Did you choose this method?","choice_items":["Yes","No"]},
+							{"type":"text","question_text":"Why did you choose or not choose this method?"}
+							]
+				}
+	survey4 = 	{"survey_name":"I8 Survey- Module 4: Build",
+				"pre_questions":["What's your name?"],
+				"methods":["Live Prototyping","Wireframe","Rapid Prototyping","Laser Cutting","3-D Printing","Water Jet Cutting","Direct Shell Production Casting","Laminated Object Manufacturing","Fused Deposition Models","Tangible Prototype","Experience Prototype","Service Prototype","Additive Manufacturing"],
+				"questions":[
+							{"type":"multiple_choice","question_text":"Were you familiar with this method before using theDesignExchange?","choice_items":["Yes","No"]},
+							{"type":"multiple_choice","question_text":"Did you choose this method?","choice_items":["Yes","No"]},
+							{"type":"text","question_text":"Why did you choose or not choose this method?"}
+							]
+				}	
+	survey5 = 	{"survey_name":"I9 Survey- Module 5: Communicate",
+				"pre_questions":["What's your name?"],
+				"methods":["Envisionment Videos","Storyboards","Service Blueprint","Business Model Canvas","7 Ps Framework","Usability Report","Personas","Composite Characters"],
+				"questions":[
+							{"type":"multiple_choice","question_text":"Were you familiar with this method before using theDesignExchange?","choice_items":["Yes","No"]},
+							{"type":"multiple_choice","question_text":"Did you choose this method?","choice_items":["Yes","No"]},
+							{"type":"text","question_text":"Why did you choose or not choose this method?"}
+							]
+				}
+	print Scriptor(survey5).scriptRender()
 
 		
--- templates/designmethod.gs
+++ templates/designmethod.gs
@@ -1,19 +1,25 @@
 function myFunction() {
-  var form = FormApp.create("{{survey_name}}"); 
-  {% for pre_quesiton in pre_questions %}form.addTextItem().setTitle("{{pre_quesiton}}");
+  var form = FormApp.create("{{survey_name}}").setDescription("In this individual survey, you will be required to submit 3 methods that you plan to select for your project. Additionally, you will be required to justify both methods that you and did not choose. Due *******"); 
+  form.setCollectEmail(true);
+  form.setProgressBar(true);
+  {% for pre_quesiton in pre_questions %}form.addTextItem().setTitle("{{pre_quesiton}}").setRequired(true);
   {% endfor %}
   {% for method in methods %}//section for {{method}}
   form.addPageBreakItem().setTitle("{{method}}");
   {% for question in questions %}
-  {% if question.type == "Text" %}form.addTextItem().setTitle("{{question.question_text}}");
-  {% elif question.type == "Multiple Choice" %}var item = form.addMultipleChoiceItem();
+  {% if question.type == "text" %}form.addTextItem().setTitle("{{question.question_text}}").setRequired(true);
+  {% elif question.type == "multiple_choice" %}var item = form.addMultipleChoiceItem();
   item.setTitle("{{question.question_text}}");
   item.setChoices([
   item.createChoice("Yes"),
   item.createChoice("No"),
   ]);
+  item.setRequired(true)
   {% endif %}{% endfor %}//end section for {{method}}
-  
-  {% endfor %}}
+  {% endfor %}
+  form.addPageBreakItem().setTitle("Additional?").setHelpText("If you used any methods that were not listed, please include them here.")
+  form.addParagraphTextItem().setTitle("Please list all additional methods here, if applicable.")
+  form.addParagraphTextItem().setTitle("Why did you choose?")
+  }
 
 
